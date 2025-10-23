7ad7253c73b2e89df73837117b3531836a0b55cd
arislemmenes <aris.lemmenes@gmail.com>
2025-10-22 22:13:25 -0700

    first commit

---
+++ include/Dice.h
@@ -0,0 +1,24 @@
+//
+// Created by Aris Lemmenes on 10/22/25.
+//
+
+#ifndef PACMAN_ENUMS_H
+#define PACMAN_ENUMS_H
+
+enum class GhostColor
+{
+    RED,
+    BLUE,
+    GREEN,
+    PINK,
+};
+
+enum class Direction
+{
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+};
+
+#endif //PACMAN_ENUMS_H
